622182599af8694a78d8d7af833a12c03b257f0a
Atang Fino Mokamogo <114358338+AtangfMokamogo@users.noreply.github.com>
2022-12-13 16:17:50 +0200

    added our custom strtok function file
    include statements for the main header file has been commented out

---
+++ _strtok.c
@@ -0,0 +1,39 @@
+#include <string.h>
+/* #include "main.h"*/
+
+/**
+  * _strtok- This is our custom implementation of the built in strtok() function
+  *
+  * @BUFFER_STR: A pointer to the input string from stdin.
+  * @delims: A pointer to the array of delimiters.
+  * 
+  * Return: a pointer to the token identified.
+  */
+
+char* _strtok(char *BUFFER_STR, const char *delims)
+{
+	/** we initialise a static variable to store the current position in the
+	  * string being tokenised, so subsequent calls to the function can
+	  * continue tokenising the same string
+	  */
+	static char *tmp_str = NULL;
+
+	if (BUFFER_STR != NULL)
+		tmp_str = BUFFER_STR;
+	else if (tmp_str == NULL)
+		return NULL;
+
+	/* store the current position in the string being tokenised */
+	char *start = tmp_str;
+
+	/* we use the strpbrk() to find the first occurence of any of the delimiters in str */
+	tmp_str = strpbrk(start, delims);
+
+	if (tmp_str != NULL)
+	{
+		*tmp_str = '\0';
+		tmp_str++;
+	}
+
+	return start;
+}
